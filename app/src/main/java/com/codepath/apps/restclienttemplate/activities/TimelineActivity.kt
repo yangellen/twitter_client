d983a26d29017b241f29ac0421d87bc15a7a00a2
Ellen Yang <yange@oregonstate.edu>
2022-03-12 11:29:51 -0800

    feat: add compose icon in actionbar

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.kt
@@ -3,6 +3,9 @@ package com.codepath.apps.restclienttemplate.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -51,6 +54,20 @@ class TimelineActivity : AppCompatActivity() {
         rvTweets.adapter = adapter
 
         populateHomeTimeline()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    // handles clicks on menu item
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.compose){
+            Toast.makeText(this, "Ready to compose tweet!", Toast.LENGTH_SHORT)
+                .show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     fun populateHomeTimeline(){
